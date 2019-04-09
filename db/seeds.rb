6ed62c0a93ecdbd8afdd3e6793626bda0952e73c
256hz <a.d@256hz.com>
2019-04-09 08:58:27 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.create(name: "Das Racist")
+Artist.create(name: "Iron & Wine")
+Artist.create(name: "ho99o9")
+
+Song.create(title: "Michael Jackson", artist_id: 1)
+Song.create(title: "Taco Bell", artist_id: 1)
+Song.create(title: "Bird Stealing Bread", artist_id: 2)
